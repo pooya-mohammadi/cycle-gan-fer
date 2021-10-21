bc61d5f7b8a6e017b369c69230c2a56c08c37215
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-10-21 22:50:09 +0330

    Fix cycle-coef's double backward error

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ from generator_model import Generator
 from deep_utils import show_destroy_cv2
 
 
-def train_fn(disc_R, disc_T, gen_RT, gen_TR, loader, opt_disc, opt_gen, l1, mse, d_scaler, g_scaler, epoch, mlflow):
+def train_fn(disc_R, disc_T, gen_RT, gen_TR, loader, opt_disc, opt_gen, l1, mse, epoch, mlflow):
     disc_R.train()
     disc_T.train()
     gen_TR.train()
@@ -33,31 +33,31 @@ def train_fn(disc_R, disc_T, gen_RT, gen_TR, loader, opt_disc, opt_gen, l1, mse,
         reference = reference.to(config.DEVICE)
 
         # Train Discriminators H and Z
-        with torch.cuda.amp.autocast():
-            toggle_grad(disc_R, disc_T, True)
-            fake_reference = gen_TR(target)
-            # d_r results
-            D_R_real = disc_R(reference)
-            D_R_fake = disc_R(fake_reference.detach())
+        # with torch.cuda.amp.autocast():
+        toggle_grad(disc_R, disc_T, True)
+        fake_reference = gen_TR(target)
+        # d_r results
+        D_R_real = disc_R(reference)
+        D_R_fake = disc_R(fake_reference.detach())
 
-            # d_r losses
-            D_R_real_loss = mse(D_R_real, torch.ones_like(D_R_real))
-            D_R_fake_loss = mse(D_R_fake, torch.zeros_like(D_R_fake))
-            D_R_loss = D_R_real_loss + D_R_fake_loss
+        # d_r losses
+        D_R_real_loss = mse(D_R_real, torch.ones_like(D_R_real))
+        D_R_fake_loss = mse(D_R_fake, torch.zeros_like(D_R_fake))
+        D_R_loss = D_R_real_loss + D_R_fake_loss
 
-            fake_target = gen_RT(reference)
+        fake_target = gen_RT(reference)
 
-            # d_t results
-            D_T_real = disc_T(target)
-            D_T_fake = disc_T(fake_target.detach())
+        # d_t results
+        D_T_real = disc_T(target)
+        D_T_fake = disc_T(fake_target.detach())
 
-            # d_t losses
-            D_T_real_loss = mse(D_T_real, torch.ones_like(D_T_real))
-            D_T_fake_loss = mse(D_T_fake, torch.zeros_like(D_T_fake))
-            D_T_loss = D_T_real_loss + D_T_fake_loss
+        # d_t losses
+        D_T_real_loss = mse(D_T_real, torch.ones_like(D_T_real))
+        D_T_fake_loss = mse(D_T_fake, torch.zeros_like(D_T_fake))
+        D_T_loss = D_T_real_loss + D_T_fake_loss
 
-            # put it together
-            D_loss = (D_R_loss + D_T_loss) / 2
+        # put it together
+        D_loss = (D_R_loss + D_T_loss) / 2
 
         # get the metrics
         # disc + reference loss
@@ -72,57 +72,55 @@ def train_fn(disc_R, disc_T, gen_RT, gen_TR, loader, opt_disc, opt_gen, l1, mse,
         mlflow.log_metric("D_loss", D_loss.item(), step=step_num)
 
         opt_disc.zero_grad()
-        d_scaler.scale(D_loss).backward()
-        d_scaler.step(opt_disc)
-        d_scaler.update()
+        D_loss.backward()
+        opt_disc.step()
 
         # Train Generators H and Z
-        with torch.cuda.amp.autocast():
-            # adversarial loss for both generators
-            toggle_grad(disc_R, disc_T, False)
-            D_R_fake = disc_R(fake_reference)
-            D_T_fake = disc_T(fake_target)
+        # adversarial loss for both generators
+        toggle_grad(disc_R, disc_T, False)
+        D_R_fake = disc_R(fake_reference)
+        D_T_fake = disc_T(fake_target)
 
-            loss_G_TR = mse(D_R_fake, torch.ones_like(D_R_fake))
-            loss_G_RT = mse(D_T_fake, torch.ones_like(D_T_fake))
+        loss_G_TR = mse(D_R_fake, torch.ones_like(D_R_fake))
+        loss_G_RT = mse(D_T_fake, torch.ones_like(D_T_fake))
 
-            # cycle loss
-            cycle_target = gen_RT(fake_reference)
-            cycle_reference = gen_TR(fake_target)
-            cycle_target_loss = l1(target, cycle_target) * config.CYCLE_LOSS_COEFFICIENT
-            cycle_reference_loss = l1(reference, cycle_reference) * config.CYCLE_LOSS_COEFFICIENT
+        # cycle loss
+        cycle_target = gen_RT(fake_reference)
+        cycle_reference = gen_TR(fake_target)
+        cycle_target_loss = l1(target, cycle_target) * config.CYCLE_LOSS_COEFFICIENT
+        cycle_reference_loss = l1(reference, cycle_reference) * config.CYCLE_LOSS_COEFFICIENT
 
-            # identity loss (remove these for efficiency if you set lambda_identity=0)
-            # if config.LAMBDA_IDENTITY:
-            #     identity_target = gen_RT(target)
-            #     identity_reference = gen_TR(reference)
-            #     identity_target_loss = l1(target, identity_target)
-            #     identity_reference_loss = l1(reference, identity_reference)
-            # else:
-            #     identity_target_loss = 0
-            #     identity_reference_loss = 0
+        # identity loss (remove these for efficiency if you set lambda_identity=0)
+        # if config.LAMBDA_IDENTITY:
+        #     identity_target = gen_RT(target)
+        #     identity_reference = gen_TR(reference)
+        #     identity_target_loss = l1(target, identity_target)
+        #     identity_reference_loss = l1(reference, identity_reference)
+        # else:
+        #     identity_target_loss = 0
+        #     identity_reference_loss = 0
 
-            # identity losspiq
-            # gen_tr_identity = 1 / mse(target, fake_reference) * config.LAMBDA_GEN_IDENTITY
-            # gen_rt_identity = 1 / mse(reference, fake_target) * config.LAMBDA_GEN_IDENTITY
-            # add all together
+        # identity losspiq
+        # gen_tr_identity = 1 / mse(target, fake_reference) * config.LAMBDA_GEN_IDENTITY
+        # gen_rt_identity = 1 / mse(reference, fake_target) * config.LAMBDA_GEN_IDENTITY
+        # add all together
 
-            G_loss_execpt_cycle = (
-                    loss_G_RT +
-                    loss_G_TR
+        G_loss_execpt_cycle = (
+                loss_G_RT +
+                loss_G_TR
+        )
+        cycle_coef = D_loss / G_loss_execpt_cycle
+        cycle_coef = cycle_coef.detach()
+        cycle_target_loss *= cycle_coef
+        cycle_reference_loss *= cycle_coef
 
-            )
-            cycle_coef = D_loss / G_loss_execpt_cycle
-            cycle_target_loss *= cycle_coef
-            cycle_reference_loss *= cycle_coef
+        G_loss = (
+                loss_G_RT +
+                loss_G_TR +
+                cycle_target_loss +
+                cycle_reference_loss
 
-            G_loss = (
-                    loss_G_RT +
-                    loss_G_TR +
-                    cycle_target_loss +
-                    cycle_reference_loss
-
-            )
+        )
         # get the metrics
         # gen loss
         mlflow.log_metric("loss_G_TR", loss_G_TR.item(), step=step_num)
@@ -150,9 +148,8 @@ def train_fn(disc_R, disc_T, gen_RT, gen_TR, loader, opt_disc, opt_gen, l1, mse,
             mlflow.log_artifact(target_path, join('images', target_path))
 
         opt_gen.zero_grad()
-        g_scaler.scale(G_loss).backward()
-        g_scaler.step(opt_gen)
-        g_scaler.update()
+        G_loss.backward()
+        opt_gen.step()
 
 
 def toggle_grad(disc_R, disc_T, grad):
@@ -189,24 +186,24 @@ def valid_fn(disc_R, disc_T, gen_RT, gen_TR, loader, l1, mse, epoch, mlflow):
         show_destroy_cv2(reference_img[0], win_name='reference')
 
         # Train Discriminators H and Z
-        with torch.cuda.amp.autocast():
-            fake_reference = gen_TR(target)
-            D_R_real = disc_R(reference)
-            D_R_fake = disc_R(fake_reference.detach())
+        # with torch.cuda.amp.autocast():
+        fake_reference = gen_TR(target)
+        D_R_real = disc_R(reference)
+        D_R_fake = disc_R(fake_reference.detach())
 
-            D_R_real_loss = mse(D_R_real, torch.ones_like(D_R_real))
-            D_R_fake_loss = mse(D_R_fake, torch.zeros_like(D_R_fake))
-            D_R_loss = D_R_real_loss + D_R_fake_loss
+        D_R_real_loss = mse(D_R_real, torch.ones_like(D_R_real))
+        D_R_fake_loss = mse(D_R_fake, torch.zeros_like(D_R_fake))
+        D_R_loss = D_R_real_loss + D_R_fake_loss
 
-            fake_target = gen_RT(reference)
-            D_T_real = disc_T(target)
-            D_T_fake = disc_T(fake_target.detach())
-            D_T_real_loss = mse(D_T_real, torch.ones_like(D_T_real))
-            D_T_fake_loss = mse(D_T_fake, torch.zeros_like(D_T_fake))
-            D_T_loss = D_T_real_loss + D_T_fake_loss
+        fake_target = gen_RT(reference)
+        D_T_real = disc_T(target)
+        D_T_fake = disc_T(fake_target.detach())
+        D_T_real_loss = mse(D_T_real, torch.ones_like(D_T_real))
+        D_T_fake_loss = mse(D_T_fake, torch.zeros_like(D_T_fake))
+        D_T_loss = D_T_real_loss + D_T_fake_loss
 
-            # put it togethor
-            D_loss = (D_R_loss + D_T_loss) / 2
+        # put it togethor
+        D_loss = (D_R_loss + D_T_loss) / 2
 
         # get the metrics
         # disc + reference loss
@@ -221,50 +218,51 @@ def valid_fn(disc_R, disc_T, gen_RT, gen_TR, loader, l1, mse, epoch, mlflow):
         mlflow.log_metric("val_D_loss", D_loss.item(), step=step_num)
 
         # Train Generators H and Z
-        with torch.cuda.amp.autocast():
-            # adversarial loss for both generators
-            D_R_fake = disc_R(fake_reference)
-            D_T_fake = disc_T(fake_target)
-            loss_G_TR = mse(D_R_fake, torch.ones_like(D_R_fake))
-            loss_G_RT = mse(D_T_fake, torch.ones_like(D_T_fake))
+        # with torch.cuda.amp.autocast():
+        # adversarial loss for both generators
+        D_R_fake = disc_R(fake_reference)
+        D_T_fake = disc_T(fake_target)
+        loss_G_TR = mse(D_R_fake, torch.ones_like(D_R_fake))
+        loss_G_RT = mse(D_T_fake, torch.ones_like(D_T_fake))
 
-            # cycle loss
-            cycle_target = gen_RT(fake_reference)
-            cycle_reference = gen_TR(fake_target)
-            cycle_target_loss = l1(target, cycle_target) * config.CYCLE_LOSS_COEFFICIENT
-            cycle_reference_loss = l1(reference, cycle_reference) * config.CYCLE_LOSS_COEFFICIENT
+        # cycle loss
+        cycle_target = gen_RT(fake_reference)
+        cycle_reference = gen_TR(fake_target)
+        cycle_target_loss = l1(target, cycle_target) * config.CYCLE_LOSS_COEFFICIENT
+        cycle_reference_loss = l1(reference, cycle_reference) * config.CYCLE_LOSS_COEFFICIENT
 
-            # # identity loss (remove these for efficiency if you set lambda_identity=0)
-            # if config.LAMBDA_IDENTITY:
-            #     identity_target = gen_RT(target)
-            #     identity_reference = gen_TR(reference)
-            #     identity_target_loss = l1(target, identity_target)
-            #     identity_reference_loss = l1(reference, identity_reference)
-            # else:
-            #     identity_target_loss = 0
-            #     identity_reference_loss = 0
+        # # identity loss (remove these for efficiency if you set lambda_identity=0)
+        # if config.LAMBDA_IDENTITY:
+        #     identity_target = gen_RT(target)
+        #     identity_reference = gen_TR(reference)
+        #     identity_target_loss = l1(target, identity_target)
+        #     identity_reference_loss = l1(reference, identity_reference)
+        # else:
+        #     identity_target_loss = 0
+        #     identity_reference_loss = 0
 
-            # identity loss
-            # gen_tr_identity = 1 / mse(target, fake_reference) * config.LAMBDA_GEN_IDENTITY
-            # gen_rt_identity = 1 / mse(reference, fake_target) * config.LAMBDA_GEN_IDENTITY
+        # identity loss
+        # gen_tr_identity = 1 / mse(target, fake_reference) * config.LAMBDA_GEN_IDENTITY
+        # gen_rt_identity = 1 / mse(reference, fake_target) * config.LAMBDA_GEN_IDENTITY
 
-            # add all together
-            G_loss_execpt_cycle = (
-                    loss_G_RT +
-                    loss_G_TR
+        # add all together
+        G_loss_execpt_cycle = (
+                loss_G_RT +
+                loss_G_TR
 
-            )
-            cycle_coef = D_loss / G_loss_execpt_cycle
-            cycle_target_loss *= cycle_coef
-            cycle_reference_loss *= cycle_coef
+        )
+        cycle_coef = D_loss / G_loss_execpt_cycle
+        cycle_coef = cycle_coef.detach()
+        cycle_target_loss *= cycle_coef
+        cycle_reference_loss *= cycle_coef
 
-            G_loss = (
-                    loss_G_RT
-                    + loss_G_TR
-                    + cycle_target_loss
-                    + cycle_reference_loss
+        G_loss = (
+                loss_G_RT
+                + loss_G_TR
+                + cycle_target_loss
+                + cycle_reference_loss
 
-            )
+        )
 
         # get the metrics
         # gen loss
@@ -294,7 +292,7 @@ def valid_fn(disc_R, disc_T, gen_RT, gen_TR, loader, l1, mse, epoch, mlflow):
             mlflow.log_artifact(target_path, join('images', target_path))
 
 
-def main(mlflow_source='./mlruns'):
+def main(mlflow_source='./mlruns', save_dir=''):
     mlflow.set_tracking_uri(mlflow_source)
     mlflow.set_experiment(config.MLFLOW_EXP)
     mlflow.start_run()
@@ -319,16 +317,16 @@ def main(mlflow_source='./mlruns'):
 
     if config.LOAD_MODEL:
         load_checkpoint(
-            config.CHECKPOINT_GEN_R, gen_TR, opt_gen, config.GEN_LEARNING_RATE,
+            join(save_dir, config.CHECKPOINT_GEN_R), gen_TR, opt_gen, config.GEN_LEARNING_RATE,
         )
         load_checkpoint(
-            config.CHECKPOINT_GEN_T, gen_RT, opt_gen, config.GEN_LEARNING_RATE,
+            join(save_dir, config.CHECKPOINT_GEN_T), gen_RT, opt_gen, config.GEN_LEARNING_RATE,
         )
         load_checkpoint(
-            config.CHECKPOINT_DISC_R, disc_R, opt_disc, config.DIS_LEARNING_RATE,
+            join(save_dir, config.CHECKPOINT_DISC_R), disc_R, opt_disc, config.DIS_LEARNING_RATE,
         )
         load_checkpoint(
-            config.CHECKPOINT_DISC_T, disc_T, opt_disc, config.DIS_LEARNING_RATE,
+            join(save_dir, config.CHECKPOINT_DISC_T), disc_T, opt_disc, config.DIS_LEARNING_RATE,
         )
 
     dataset = ReferenceTargetDataset(
@@ -353,19 +351,19 @@ def main(mlflow_source='./mlruns'):
         num_workers=config.NUM_WORKERS,
         pin_memory=True
     )
-    g_scaler = torch.cuda.amp.GradScaler()
-    d_scaler = torch.cuda.amp.GradScaler()
+    # g_scaler = torch.cuda.amp.GradScaler()
+    # d_scaler = torch.cuda.amp.GradScaler()
     if config.TRAIN:
         for epoch in range(config.NUM_EPOCHS):
             train_fn(disc_R, disc_T, gen_RT, gen_TR, loader, opt_disc,
-                     opt_gen, L1, mse, d_scaler, g_scaler, epoch, mlflow)
+                     opt_gen, L1, mse, epoch, mlflow)
             # valid_fn(disc_R, disc_T, gen_RT, gen_TR, val_loader, L1, mse, epoch, mlflow)
 
             if config.SAVE_MODEL:
-                save_checkpoint(gen_TR, opt_gen, filename=config.CHECKPOINT_GEN_R)
-                save_checkpoint(gen_RT, opt_gen, filename=config.CHECKPOINT_GEN_T)
-                save_checkpoint(disc_R, opt_disc, filename=config.CHECKPOINT_DISC_R)
-                save_checkpoint(disc_T, opt_disc, filename=config.CHECKPOINT_DISC_T)
+                save_checkpoint(gen_TR, opt_gen, filename=join(save_dir, config.CHECKPOINT_GEN_R))
+                save_checkpoint(gen_RT, opt_gen, filename=join(save_dir, config.CHECKPOINT_GEN_T))
+                save_checkpoint(disc_R, opt_disc, filename=join(save_dir, config.CHECKPOINT_DISC_R))
+                save_checkpoint(disc_T, opt_disc, filename=join(save_dir, config.CHECKPOINT_DISC_T))
     else:
         for epoch in range(config.TEST_EPOCHS):
             valid_fn(disc_R, disc_T, gen_RT, gen_TR, val_loader, L1, mse, epoch, mlflow)
